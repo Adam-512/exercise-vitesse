a3cdd68e5e70295d7cf9a17ab14ee1709850dd26
chenhui <512006634@qq.com>
2021-10-30 00:46:29 +0800

    fix ssr build:window not defined

--- src/modules/permission.ts
+++ src/modules/permission.ts
@@ -2,11 +2,16 @@ import { UserModule } from '~/types'
 
 export const install: UserModule = ({ router }) => {
   router.beforeEach((to, from, next) => {
-    const isRequireAuth = to.meta?.requireAuth
-    const sessionId = sessionStorage.getItem('sessionId')
-    if (isRequireAuth && !sessionId)
-      router.push('/404')
-    else
-      next()
+    const isRequireAuth = to?.meta?.requireAuth
+    try {
+      const sessionId = window.sessionStorage.getItem('sessionId')
+      if (isRequireAuth && !sessionId)
+        router.push('/404')
+      else
+        next()
+    }
+    catch (error) {
+
+    }
   })
 }
